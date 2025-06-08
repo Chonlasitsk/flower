c4999bde22b5b4fcdf24c88bb287ab6c91daa2cd
chonlasit <chonlasit@iapp.co.th>
2025-06-09 01:18:02 +0700

    task dict not in scope when all task-id cached

--- flower/views/tasks.py
+++ flower/views/tasks.py
@@ -126,8 +126,8 @@ class TasksDataTable(BaseHandler):
         filtered_tasks = [None] * len(sorted_tasks_paginated)
         for idx, (task, task_from_redis, task_ttl) in enumerate(zip(sorted_tasks_paginated, tasks_from_redis_dict, task_ttls)):
             task_id = task[0]
+            task_dict = as_dict(self.format_task(task)[1])
             if task_id not in self.application.task_data_cache:
-                task_dict = as_dict(self.format_task(task)[1])
                 if task_dict.get('worker'):
                     task_dict['worker'] = task_dict['worker'].hostname
 
